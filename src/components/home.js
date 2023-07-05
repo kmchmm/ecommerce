7afbe99581d9f9930bdb2a49aae7b9f52780d8c0
kmchmm <113099254+kmchmm@users.noreply.github.com>
2023-07-06 04:00:37 +0800

    update

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+
 import '../App.css';
 
 export const Home = () => {
     return (
         <div className='App'>
-            <div className='home-container'>
+            <div className='home-container d-flex align-items-center justify-content-center'>
                 <h1>Home</h1>
+                <div className='underline'></div>
+                <div className='d-flex justify-content-center'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <Link to="/shop">Shop Now</Link>
+                </div>
             </div>
         </div>
     );
